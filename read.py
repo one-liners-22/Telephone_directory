4e076e8d0f801c97b2d66235e8045319161efd75
Valeriy <cherkasov77@mail.ru>
2022-10-16 23:43:51 +0400

    Добавлен метод чтения из файла txt с преобразованием в список словарей Valera

--- read.py
+++ read.py
@@ -1,6 +1,12 @@
-data = [{"id": "0000001", "secondname": "Писарев", "name": "Николай", "lastname": -1, "comment": ""}, {"id": "0000001", "secondname": "Писарев", "name": "Николай", "lastname": -1, "comment": ""}]
-# ["0000001", "Писарев", "Николай", -1, ""]["0000001", "Писарев", "Николай", -1, ""]
-
-
 def get_data ():
-    return data
+
+    dicts_from_file = []
+    name_symbol=["id","secondname", "name", "lastname", "comment"]
+    with open("bd.txt", "r",encoding="utf-8") as bd:
+        
+        for line in bd:
+            dicts_from_file.append(dict(zip(name_symbol, line.split("::"))))
+
+    return dicts_from_file
+
+    
