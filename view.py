7b84e1bd311ea60261feae3249659683eee427f7
Gaiah <gaiah.a.akopian@gmail.com>
2022-10-17 22:00:15 +0400

    Added find_contact and create_contact methods

--- view.py
+++ view.py
@@ -6,7 +6,32 @@ def user_command():                                              ## Ф, кото
         print("Incorrect number! The programm will be closed.")
         return 4
 
-# print(user_command())
+# # print(user_command())
+
+
+def find_contact():
+    find_me = input('Input contacts firstname: ')
+    return find_me
+
+
+
+def create_contact():
+    new_contact = {'secondname': input('Input Secondname or press "enter" in case if you do not want to add it: ')}
+    new_contact['name'] =  input('Input Firstname: ')
+    while new_contact['name'] == '':
+        new_contact['name'] =  input('Input Firstname: ')
+    new_contact['lastname'] = input('Input Lastname or press "enter" in case if you do not want to add it: ')
+    new_contact['phone_number'] = int(input('Input phone number without + sign: '))
+    print(type(new_contact.get('phone_number')))
+    #while new_contact.get('phone_number') == :
+        #new_contact['phone_number'] = input('Input phone number without "+" sign: ')
+    new_contact['comment'] = input('Leave a comment here or press "enter" in case if you do not need it: ')
+    return new_contact
+
+#print(create_contact())
+
+
+
 
 
 from tabulate import tabulate                                       ## Таблица для вывода всех контактов
@@ -24,4 +49,4 @@ def print_all_contacts(data):
 
     print(tabulate(data_to_print, headers=col_names, tablefmt="fancy_grid", showindex="never"))
 
-#print_all_contacts(data)
+# #print_all_contacts(data)
