8296e9420b609d95083829aa08d146b46e88a43a
Nikolay Pisarev <pisarev82@yandex.ru>
2022-10-19 01:37:36 +0300

    Рабочая версия 0.3

--- Record.py
+++ Record.py
@@ -1,28 +1,8 @@
-# Актуальный код
 
-def save_data (for_rec):
-    src = 'dataBase.txt'
-    convert_data = []
-    for i in for_rec:
-        convert_data.append(i)
-        save_convert_data_str = '::'.join(convert_data)
-        with open(src, 'a', encoding = 'utf-8') as file:
-              file.writeline(save_convert_data_str)
-        print(f'Контакт {save_convert_data_str} сохранен в {src}')
-    return save_convert_data_str
-
-
-
-# пример
-
-# src = "dataBase.txt"
-# def save_data(for_rec):
-    
-#     conver_data = " " 
-
-#     # with open(src, "a",encoding="utf-8") as file:
-#     #     file.writeline(conver_data)
-#     print(f"Контакт {conver_data}, сохранен в {src}")
-
-# Образец for_rec {'id': '0000007', 'secondname': 'Ли', 'name': 'Борис', 'lastname': '-1', 'Phone number': '-1', 'comment': 'Обработка, controller\n'}
-# Образец conver_data = "0000010::Яковлев::Александр::Михайлович::+79507355238::-1" +"\n"
+def save_data(data):
+    print(type(data))
+    src = "dataBase.txt"
+    with open(src, "w", encoding="utf-8") as file:
+        for i in data:
+            file.writelines("::".join( i.values())) 
+       
--- view.py
+++ view.py
@@ -1,5 +1,5 @@
 def user_command():                                              ## Ф, которую запрашивает контроллер для цифры юзера
-    num = int(input("Input \n 1. If You want to find a contact \n 2. If You want to create a new contact \n 3. If You want to see all contacts:   \n 4. If You want to close the programm: \n"))
+    num = int(input("Input \n 1. If You want to find a contact: \n 2. If You want to create a new contact: \n 3. If You want to see all contacts:   \n 4. If You want to close the programm: \n"))
     if 0 < num < 5:
         return num
     else:
@@ -10,27 +10,25 @@ def user_command():                                              ## Ф, кото
 
 
 def find_contact():
-    find_me = input('Input contacts firstname: ')
+    find_me = input('Input contacts firstname or lastname or middlename: ')
     return find_me
 
 
 
 def create_contact():
-    new_contact = {'secondname': input('Input Secondname or press "enter" in case if you do not want to add it: ')}
+    new_contact = {"id":""}
+    new_contact['secondname'] = input('Input Secondname or press "enter" in case if you do not want to add it: ')
     new_contact['name'] =  input('Input Firstname: ')
     while new_contact['name'] == '':
         new_contact['name'] =  input('Input Firstname: ')
     new_contact['lastname'] = input('Input Lastname or press "enter" in case if you do not want to add it: ')
-    new_contact['phone_number'] =  input('Input phone number without + sign: ')
-    print(new_contact.get('phone_number').isdigit() and new_contact.get('phone_number')[0] == "+")
-    new_contact['comment'] = input('Leave a comment here or press "enter" in case if you do not need it: ')
+    new_contact['phone_number'] =  input('Input phone number: ')
+    new_contact['comment'] = input('Leave a comment here or press "enter" in case if you do not need it: ') + "\n"
+ 
+    print(new_contact)
     return new_contact
 
 # print(create_contact())
-
-
-
-
 
 from tabulate import tabulate                                       ## Таблица для вывода всех контактов
 
@@ -45,7 +43,7 @@ def print_all_contacts(data):
        
 
     col_names = ["Firstname", "Lastname", "Middlename", "Phone number", "Comment"]
-    print(data_to_print)
+    # print(data_to_print)
     print(tabulate(data_to_print, headers=col_names, tablefmt="fancy_grid", showindex="never"))
 
 # #print_all_contacts(data)
